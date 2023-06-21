76da92551687f4abf7857581216383e0049e4abf
Slava Meyler <vmeylers@gmail.com>
2023-06-21 23:25:53 +0300

    evening

--- memeController.js
+++ memeController.js
@@ -2,20 +2,53 @@ let gCanvas
 let gCtx
 
 
-  function init() {
-    gCanvas = document.querySelector('#canvas')
-    gCtx = gCanvas.getContext('2d')
-
-    renderMeme()
-  } 
-
-
+function init() {
+    
+ gCanvas = document.querySelector('#canvas')
+ gCtx = gCanvas.getContext('2d')
+  renderImgs()
+}
 
 
-  function renderMeme() {
-    const img = new Image()
-    img.src = 'meme-imgs (square)/3.jpg'
-    img.onload = () => {
-      gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xEnd,yEnd
-    }
+
+
+
+// function renderMeme() {
+//     const img = new Image()
+//     img.src = 'meme-imgs/1.jpg'
+//     img.onload = () => {
+//         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xEnd,yEnd
+//     }
+// }
+
+
+function renderImgs() {
+    var strHTML = ''
+    for (var i = 0; i < gImgs.length; i++) {
+      
+            strHTML += `<section>
+            <img class="img"src="meme-imgs/${id}.jpg" alt=""> 
+        </section>
+         `
+
+        }
+    
+    const elContainer = document.querySelector('.board')
+    elContainer.innerHTML = strHTML
+}
+
+function renderImgs() {
+    const imgs = creatImgs()
+  
+    // const elList = document.querySelector('.place-list')
+    let strHtmls = gImgs.map(({ id }) => {
+        return `
+        <section>
+        <img class="img"src="meme-imgs/${id}.jpg" alt=""> 
+    </section>
+     `
+          
+      })
+      .join('')
+    elList.innerHTML = strHtmls
   }
--- memeService.js
+++ memeService.js
@@ -1,12 +1,42 @@
+// var gImgs = [{ id: 1, url: 'meme-imgs/1.jpg', keywords: ['funny', 'cat'] }]
+const gImgs=[]
 var gMeme = {
-    selectedImgId: 5,
+    selectedImgId: 1,
     selectedLineIdx: 0,
     lines: [
-    {
-    txt: 'I sometimes eat Falafel',
-    size: 20,
-    color: 'red'
-    }
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
     ]
-   }
-   
+}
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+
+
+function getMeme() {
+    return gMeme
+}
+
+function getImages() {
+    return gImgs
+}
+
+function setLineTxt() {
+
+
+}
+creatImgs()
+function creatImgs() {
+    for (var i = 0; i < 18; i++) {
+        const id = i + 1
+        const img = {
+            id,
+            url: `meme-imgs/${id}.jpg`,
+            keywords: []
+        }
+        gImgs.push(img)
+    }
+console.log(gImgs)
+}
